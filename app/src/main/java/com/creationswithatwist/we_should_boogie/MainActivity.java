9364c65e0a9493fc2fcbaaac830262cd90d5c81b
StinoBino <s.r.h.arnold@gmail.com>
2018-06-14 20:59:27 +0200

    Players and Start v1

--- app/src/main/java/com/creationswithatwist/we_should_boogie/MainActivity.java
+++ app/src/main/java/com/creationswithatwist/we_should_boogie/MainActivity.java
@@ -1,26 +1,48 @@
 package com.creationswithatwist.we_should_boogie;
 
-import android.drm.DrmStore;
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout linearWrapper;
-
+    int adjactivePlayerOne;
+    int adjactivePlayerTwo;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         linearWrapper = (LinearLayout) findViewById(R.id.linear_wrapper);
+
+        Random rand = new Random();
+        adjactivePlayerOne = rand.nextInt(0 - 4);
+        adjactivePlayerTwo = rand.nextInt(0 - 4);
+        while (adjactivePlayerOne == adjactivePlayerTwo) {
+            adjactivePlayerTwo = rand.nextInt(0 - 4);
+        }
+
+        final EditText inputPlayerOne = new EditText(this.getApplicationContext());
+        final EditText inputPlayerTwo = new EditText(this.getApplicationContext());
         Button button = new Button(this.getApplicationContext());
-
-        Card card1 = new Card("black");
-
-        button.setText(card1.getAction());
-
+        button.setText("Let's Boogie");
         linearWrapper.addView(button);
+
+        button.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Player player1 = new Player(inputPlayerOne.getText().toString(), adjactivePlayerOne);
+                        Player player2 = new Player(inputPlayerTwo.getText().toString(), adjactivePlayerTwo);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/creationswithatwist/we_should_boogie/Player.java
@@ -0,0 +1,15 @@
+package com.creationswithatwist.we_should_boogie;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class Player {
+
+    private String name;
+    private String[] Adjective = new String[]{"Cute", "Cuddly", "Sweet", "Tough", "Funny"};
+
+    public Player (String name, int i) {
+        this.name = this.Adjective[i] + " " + name;
+    }
+
+}
