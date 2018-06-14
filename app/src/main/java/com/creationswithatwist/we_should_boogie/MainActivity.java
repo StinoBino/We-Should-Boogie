6c946f3268504c34d55545efa8122be2be8f2707
Rivingo <Rivingo@users.noreply.github.com>
2018-06-14 19:09:57 +0200

    empty_project

---
+++ app/src/main/java/com/creationswithatwist/we_should_boogie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.creationswithatwist.we_should_boogie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
